6b99ab95496e94ea443aa9f50a51cd5c4de7968d
Shubham Gurav <shubham.gurav@verval.com>
2026-04-23 15:36:12 +0530

    fixed redis config

--- src/main/java/com/chatapp/chat/config/RedisConfig.java
+++ src/main/java/com/chatapp/chat/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.chatapp.chat.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -10,43 +11,50 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.net.URI;
 import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-
-    @Value("${spring.data.redis.password}")
-    private String redisPassword;
+    @Value("${spring.data.redis.url}")
+    private String redisUrl;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
+
+        URI uri = URI.create(redisUrl);
+
         RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-        redisConfig.setHostName(redisHost);
-        redisConfig.setPort(redisPort);
-        redisConfig.setPassword(redisPassword);
+        redisConfig.setHostName(uri.getHost());
+        redisConfig.setPort(uri.getPort());
+
+        // Extract password
+        if (uri.getUserInfo() != null) {
+            String password = uri.getUserInfo().split(":", 2)[1];
+            redisConfig.setPassword(RedisPassword.of(password));
+        }
 
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
                 .commandTimeout(Duration.ofSeconds(60))
-                .useSsl()
+                .useSsl() // IMPORTANT for rediss://
                 .build();
 
         return new LettuceConnectionFactory(redisConfig, clientConfig);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+
+        template.setConnectionFactory(connectionFactory);
+
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         template.afterPropertiesSet();
         return template;
     }
